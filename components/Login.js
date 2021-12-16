02a7ff3cf7d1a9c5f5bab3e4cfaad0d395392709
vinayak <vinayaks0031@gmail.com>
2021-12-16 19:30:30 +0530

    login with moralis completed

--- components/Login.js
+++ components/Login.js
@@ -1,16 +1,20 @@
 import Image from 'next/image';
 import { useMoralis } from 'react-moralis';
 export default function Login() {
-    const { authenticate }=useMoralis();
+    const { authenticate, isAuthenticating } = useMoralis();
     return (
         <>
-            <div className="z-1">
-            <h1>login </h1>
-            <button onClick={authenticate}>Login</button> 
-            </div>
-            <div>
-
-            <Image src="/bg1.png" layout='fill'/>
+            <div className="bg-black relative">
+                <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+                    <Image className="rounded-full" src="/vr1.jpg" width={200} height={200} />
+                    {isAuthenticating ?
+                        <button className='bg-yellow-600 rounded-lg p-5 font-bold animate-pulse' onClick={authenticate}>Entering in Metaverse...</button>:
+                        <button className='bg-yellow-500 rounded-lg p-5 font-bold animate-pulse' onClick={authenticate}>Login to the Metaverse</button>
+                    }
+                </div>
+                <div className="w-full h-screen">
+                    <Image src="/BG3.jpeg" layout='fill' objectFit='cover' />
+                </div>
             </div>
         </>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
 import Head from 'next/head'
 import Login from '../components/Login'
 import { useMoralis } from 'react-moralis';
+import Image from 'next/image';
 
 export default function Home() {
-  const {isAuthenticated, logout, isInitializing } =useMoralis();
+  const {isAuthenticated, logout } =useMoralis();
 
   if(!isAuthenticated) {
     return <Login />
   }
-  
-
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div >
       <Head>
-        <title>Create Next App</title>
+        <title>Metaverse chat</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to metaverse</h1>
-      <button onClick={logout}>Logout</button>
+      <div className="bg-black relative">
+                <div className="text-white flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+                    <p  style={{fontSize:"50px",fontWeight:"500"}}>Welcome to the metaverse</p>
+                    <button className='bg-gray-900 rounded-lg p-5 font-bold animate-pulse' onClick={logout}>Logout</button>
+                </div>
+                <div className="w-full h-screen">
+                    <Image src="/bg4.jpg" layout='fill' objectFit='cover' />
+                </div>
+            </div>
     </div>
   )
 }
